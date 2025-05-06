5fc3696721a1287a3e1be2a215f22a4a1017537a
Ali-Hadi-Saker <alihady530@gmail.com>
2025-05-06 11:51:42 +0300

    added apiResources the app.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::apiResource('events', EventController::class);
+
+//Laravel will only look for an attendee that belongs to the given even
+//this route require 2 parameters (event and attendee)
+Route::apiResource('events.attendees', AttendeeController::class)
+        ->scoped(['attendee' => 'event']);

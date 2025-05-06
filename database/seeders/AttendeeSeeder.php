ee834221832531e8efc3af5901b90ea5205e10f4
Ali-Hadi-Saker <alihady530@gmail.com>
2025-05-06 17:37:38 +0300

    added event and attendee seeder files

--- database/seeders/AttendeeSeeder.php
+++ database/seeders/AttendeeSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Attendee;
+use App\Models\Event;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +15,18 @@ class AttendeeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $users = User::all();
+        $events = Event::all();
+
+        foreach($users as $user) {
+            $eventToAttend = $events->random(rand(1, 3));//get a random nb of events from 1 to 3
+
+            foreach($eventToAttend as $event) {
+                Attendee::create([
+                    'user_id' => $user->id,
+                    'event_id' => $event->id
+                ]);
+            }
+        }
     }
 }
